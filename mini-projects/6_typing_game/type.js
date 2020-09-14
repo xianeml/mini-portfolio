17f8157b4a621b8ecb9b00c68d0463225f19606b
Mihyun Lee <xianemml@gmail.com>
2020-09-14 23:31:37 +0900

    🐛 fix : Modify check button conditions - #1

--- mini-projects/6_typing_game/type.js
+++ mini-projects/6_typing_game/type.js
@@ -11,24 +11,31 @@ var wordList = ['time','year','people','way','day','man','thing','woman','life',
     'school','state','family','student','group','country','problem','hand','part','place',
     'week','company','system','program','question','work','government'];
 
-startBtn.addEventListener("click", function(){
+
+function start(){
     function randomWord(){
         var random = Math.floor(Math.random() * wordList.length);
         return wordList[random];
     }
     word.textContent = randomWord();
-});
+}
 
-checkBtn.addEventListener("click", function(){
+startBtn.addEventListener("click", start);
+
+checkBtn.addEventListener("click", function(event){
     if(input.value == word.textContent){
         comment.textContent = 'Right!!!';
         score.textContent = scoreSet+=1;
         goodSound.play();
+        start();
+        input.value = "";
     }else if(input.value){
         comment.textContent = 'Wrong!!! score-1';
         score.textContent = scoreSet-=1;
         badSound.play();
+        input.value = "";
     }else{
         comment.textContent = 'Type something!';
     }
+    event.preventDefault;
 })
